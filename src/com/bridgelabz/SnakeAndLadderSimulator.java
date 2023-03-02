d2e4b37da69048bd0c731b005439f09bfdabefcf
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-02 11:00:50 +0530

    Added: UC1 Started With Single Player At 0 Position

--- src/com/bridgelabz/SnakeAndLadderSimulator.java
+++ src/com/bridgelabz/SnakeAndLadderSimulator.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class SnakeAndLadderSimulator {
 	public static void main(String[] args) {
+		
 		System.out.println("Start the Snake and Ladder Game");
 		System.out.println("Single Player at Start Position 0");
 	}
